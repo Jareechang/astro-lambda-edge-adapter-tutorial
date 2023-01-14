637fcfbc8b19efdf28d3ca4b848f04cc36b8b608
Jerry Chang <therealjerrycan@gmail.com>
2023-01-14 15:40:17 -0800

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import lambdaEdgeAdapter from '@common-web/astro-lambda-edge';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  vite: {
+    build: {
+      target: ['node16.0.0'],
+      manifest: true,
+    }
+  },
+  output: 'server',
+  adapter: lambdaEdgeAdapter()
+});
